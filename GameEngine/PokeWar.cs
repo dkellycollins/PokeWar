d36bd14763c46a73d95f35d3e356ac307c13d5cd
Devin Kelly-Collins <dkellycollins@gmail.com>
2012-04-04 22:40:00 -0500

    Renamed PokeWarEngine to GameEngine

---
+++ GameEngine/PokeWar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CardLib;
+
+namespace GameEngine
+{
+    public class PokeWar
+    {
+        private CardDeck _deck;
+        private Player _player1;
+        private Player _player2;
+
+        public static List<Card> GetAceCards()
+        {
+            return null;
+        }
+
+        public PokeWar(Player p1, Player p2)
+        {
+            _deck = new CardDeck();
+            //TODO remove the 4 ace cards
+            _player1 = p1;
+            _player2 = p2;
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public void PlayTurn()
+        {
+
+        }
+
+        public void Cleanup()
+        {
+
+        }
+    }
+}
