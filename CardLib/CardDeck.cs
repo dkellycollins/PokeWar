e808c7221fb922d046d0020e37ff7ce3a4add4ae
Alex <alexanderdonnelly2@yahoo.com>
2012-04-04 23:05:31 -0500

    I did nothing with the Card class. I modified the CardDeck class. I switched the deck from an array to a List. Completed the Draw, Shuffle, and ReturnAllCards function. Draw takes no parameters, and takes a copy of the top card and returns it lowers the topcard by 1. Shuffle takes a count parameter which is how many times it's shuffled. The Shuffle function takes a random card and swaps it with the another card. At the end, I search the List for the 4 Aces, and make them the top 4 cards. I swap the top 4 cards with the Aces. ReturnAllCards just sets the top card to 51, or the end of the list.

--- CardLib/CardDeck.cs
+++ CardLib/CardDeck.cs
@@ -1,13 +1,16 @@
-﻿namespace CardLib
+﻿using System;
+using System.Collections.Generic;
+namespace CardLib
 {
     public class CardDeck
     {
-        private Card[] deck;
+        private List<Card> deck = new List<Card>();
         private int topCard;
 
         public CardDeck()
         {
-            deck = new Card[52];
+            //Makes a deck
+            deck = new List<Card>(52);
             for(int i = 1; i <= 4; i++)
             {
                 for(int a = 1; a <= 13; a++)
@@ -16,25 +19,48 @@
                     c.SetImageSources(
                         DeckSettings.Default.FrontImgBaseName + c.ToString() + DeckSettings.Default.ImageExtention, 
                         DeckSettings.Default.BackImgSource + DeckSettings.Default.ImageExtention);
-                    deck[(i * a) - 1] = c;
+                    deck[(i * (a-1)) - 1] = c;
                 }
             }
-            topCard = 52;
         }
 
         public Card Draw()
         {
-            return null;
+            Card draw = new Card(deck[topCard]);
+            topCard--;
+            return draw;
         }
 
         public void Shuffle(int count)
-        {
-
+        {   //Returns all Cards, the shuffles them count number of times.
+            ReturnAllCards();
+            System.Random random = new System.Random();
+            for (int a = 0; a < count; a++)
+            {
+                for (int i = 0; i < deck.Count; i++)
+                {
+                    int j = random.Next(deck.Count);
+                    Card temp = deck[i];
+                    deck[i] = deck[j];
+                    deck[j] = temp;
+                }
+            }
+            topCard = 51;
+            //Finds the Aces in the deck and swaps them with the top four Cards.
+            for (int i = topCard; i >= topCard - 4; i--)
+            {
+                int index = deck.FindIndex(item => item.Rank.Equals("A"));
+                Card c = deck.Find(item => item.Rank.Equals("A"));
+                Card temp = deck[i];
+                deck[i] = c;
+                deck[index] = temp;
+            }
+            topCard = 51;
         }
 
         public void ReturnAllCards()
         {
-
+            topCard = 51;
         }
     }
 }
